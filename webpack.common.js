2600a7d83afc0a8373c542f3a12300e2660549e8
Khrabrov Aleksandr <v1ptyoz@gmail.com>
2021-08-25 17:18:49 +1000

    Migrate to webpack

---
+++ webpack.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    devtool: 'source-map',
+    optimization: {
+        usedExports: true
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                }
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: '/src/index.html'
+        }),
+        new ForkTsCheckerWebpackPlugin(),
+        // new CopyPlugin({
+        //     patterns: [
+        //         { from: 'src/fonts', to: 'fonts' },
+        //         { from: 'src/images', to: 'images' }
+        //     ]
+        // }),
+    ]
+};
